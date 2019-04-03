d634194e65bd13809407aa00bd72d1d2e3dd5edb
Nikhil Nar <ncn251@nyu.edu>
2019-04-03 18:23:11 -0400

    Most popular movies

---
+++ popular-movies.py
@@ -0,0 +1,15 @@
+from pyspark import SparkConf, SparkContext
+
+conf = SparkConf().setAppName("PopularMovie")
+sc = SparkContext(conf=conf)
+
+lines = sc.textFile("/user/ncn251/u.data")
+movies = lines.map(lambda x: (int(x.split()[1]), 1))
+movieCounts = movies.reduceByKey(lambda x, y: x+y)
+
+flipped = movieCounts.map(lambda x, y: (y, x))
+sortedMovies = flipped.sortByKey()
+results = sortedMovies.collect()
+
+for result in results:
+    print(result)
